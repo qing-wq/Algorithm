508809617c4fd1eabd1de6052a1863237de41bc6
qing-wq <2702461713@qq.com>
2022-04-01 00:47:08 +0800

    commit

---
+++ src/LeetCode/插入排序.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class 插入排序 {
+    public int[] SortArray(int[] arr) {
+        // 当前待排序的元素值
+        int currentValue;
+        for (int i = 0; i < arr.length; i++) {
+            // 已经排序好的索引
+            int preIndex = i;
+            currentValue = arr[preIndex + 1];
+            // 如果当前元素小于前一个，将前一个后移一位
+            while (currentValue < arr[preIndex] && preIndex >= 0) {
+                arr[preIndex + 1] = arr[preIndex];
+                preIndex--;
+            }
+            // 插入
+            arr[preIndex] = currentValue;
+        }
+        return arr;
+    }
+}
