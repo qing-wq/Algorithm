8c6b0c545bbc421881186fd084744124ee7a9020
qing-wq <2702461713@qq.com>
2022-04-01 19:13:00 +0800

    排序

---
+++ src/排序/希尔排序.java
@@ -0,0 +1,38 @@
+package 排序;
+
+import com.sun.org.apache.regexp.internal.REUtil;
+
+public class 希尔排序 {
+    public static int[] SortArray(int[] arr) {
+        if (arr.length == 0) {
+            return null;
+        }
+        int len = arr.length;
+        // currentValue指示当前元素，gap指示分组间隔
+        int currentValue, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                // 组内已经排序好的元素
+                int preIndex = i - gap;
+                currentValue = arr[i];
+                while (preIndex >= 0 && currentValue < arr[preIndex]) { // 这里要将preIndex >= 0放在&&前面，不然会报越界错误
+                    // 将排序好的元素后移一位
+                    arr[preIndex + gap] = arr[preIndex];
+                    preIndex -= gap;
+                }
+                // 插入当前元素
+                arr[preIndex + gap] = currentValue;
+            }
+            gap /= 2;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 2, 1,6};
+        new 希尔排序().SortArray(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
