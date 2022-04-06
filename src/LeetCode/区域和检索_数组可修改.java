97938ace0050ef840daa2f4f5e7c1b8e1292d7e7
qing-wq <2702461713@qq.com>
2022-04-06 16:12:24 +0800

    线段树

---
+++ src/LeetCode/区域和检索_数组可修改.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class 区域和检索_数组可修改 {
+    class NumArray {
+        int[] nums;
+        public NumArray(int[] nums) {
+            this.nums = nums;
+        }
+
+        // 更新数组中的元素
+        public void update(int index, int val) {
+            nums[index] = val;
+        }
+
+        // 区域和检索
+        public int sumRange(int left, int right) {
+            int sum = 0;
+            for (int i = left; i <= right; i++) {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
+}
